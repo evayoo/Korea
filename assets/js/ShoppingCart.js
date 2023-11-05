5fb7b2fe0c197ef85865449544d43f1af1380789
Chaewon <chaewon.yoo@powers.org>
2023-11-05 12:33:19 +0100

    cart update

--- assets/js/ShoppingCart.js
+++ assets/js/ShoppingCart.js
@@ -1,52 +1,66 @@
-function createCartItemElement(item, checkedItems, handleCheckChange, handleQuantityChange, handleDelete, quantity) {
-  const cartItem = document.createElement('li');
-  cartItem.className = 'cart-item-body';
+// Function to create a cart item element
+function createCartItemElement(
+  item,
+  checkedItems,
+  handleCheckChange,
+  handleQuantityChange,
+  handleDelete,
+  quantity
+) {
+  const cartItem = document.createElement("li");
+  cartItem.className = "cart-item-body";
 
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.className = 'cart-item-checkbox';
-  checkbox.addEventListener('change', (e) => {
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.className = "cart-item-checkbox";
+  checkbox.addEventListener("change", (e) => {
     handleCheckChange(e.target.checked, item.id);
   });
   checkbox.checked = checkedItems.includes(item.id);
 
-  const thumbnail = document.createElement('div');
-  thumbnail.className = 'cart-item-thumbnail';
+  const thumbnail = document.createElement("div");
+  thumbnail.className = "cart-item-thumbnail";
 
-  const img = document.createElement('img');
+  const img = document.createElement("img");
   img.src = item.img;
   img.alt = item.name;
 
   thumbnail.appendChild(img);
 
-  const info = document.createElement('div');
-  info.className = 'cart-item-info';
+  const info = document.createElement("div");
+  info.className = "cart-item-info";
 
-  const title = document.createElement('div');
-  title.className = 'cart-item-title';
-  title.setAttribute('data-testid', item.name);
+  const title = document.createElement("div");
+  title.className = "cart-item-title";
+  title.setAttribute("data-testid", item.name);
   title.textContent = item.name;
 
-  const price = document.createElement('div');
-  price.className = 'cart-item-price';
-  price.textContent = item.price + ' CHF';
+  const price = document.createElement("div");
+  price.className = "cart-item-price";
+  price.textContent = item.price + " CHF";
+
+  // Add item details to the cart item
+  const detail = document.createElement("div");
+  detail.className = "cart-item-detail";
+  detail.textContent = item.detail;
 
   info.appendChild(title);
   info.appendChild(price);
+  info.appendChild(detail);
 
-  const quantityInput = document.createElement('input');
-  quantityInput.type = 'number';
+  const quantityInput = document.createElement("input");
+  quantityInput.type = "number";
   quantityInput.min = 1;
-  quantityInput.className = 'cart-item-quantity';
+  quantityInput.className = "cart-item-quantity";
   quantityInput.value = quantity;
-  quantityInput.addEventListener('input', (e) => {
+  quantityInput.addEventListener("input", (e) => {
     handleQuantityChange(Number(e.target.value), item.id);
   });
 
-  const deleteButton = document.createElement('button');
-  deleteButton.className = 'cart-item-delete';
-  deleteButton.textContent = 'Delete';
-  deleteButton.addEventListener('click', () => {
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "cart-item-delete";
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => {
     handleDelete(item.id);
   });
 
@@ -60,137 +74,46 @@ function createCartItemElement(item, checkedItems, handleCheckChange, handleQuan
 }
 
 // Usage:
-const cart = document.getElementById('cart');
+const cart = document.getElementById("cart");
 let item = {
   id: 1,
-  img: '../../assets/images/bibim.jpg',
-  name: 'Bibimbap',
-  price: 30,
+  img: "../../assets/images/bibim.jpg",
+  name: "Bibimbap",
+  price: 30 // Add item detail
 };
 const checkedItems = [];
 const quantity = 1;
 
 console.log(item);
-handleCheckChange = function(){};
-handleQuantityChange = function(){};
-handleDelete = function(){};
-const cartItem = createCartItemElement(item, checkedItems, handleCheckChange, handleQuantityChange, handleDelete, quantity);
+handleCheckChange = function () {};
+handleQuantityChange = function () {};
+handleDelete = function () {};
+const cartItem = createCartItemElement(
+  item,
+  checkedItems,
+  handleCheckChange,
+  handleQuantityChange,
+  handleDelete,
+  quantity
+);
 cart.appendChild(cartItem);
 
-
-
-
-
-// Order summary 
+// Order summary
 
 function updateOrderSummary(totalQty, total) {
-  const totalQtyElement = document.getElementById('totalQty');
-  const totalAmountElement = document.getElementById('totalAmount');
+  const totalQtyElement = document.getElementById("totalQty");
+  const totalAmountElement = document.getElementById("totalAmount");
 
   if (totalQtyElement && totalAmountElement) {
-    totalQtyElement.textContent = totalQty + ' Items';
-    totalAmountElement.textContent = total + ' CHF';
+    totalQtyElement.textContent = totalQty + " Items";
+    totalAmountElement.textContent = total + " CHF";
   }
 }
 
 // Usage:
 const totalQty = 5; // Replace with your actual totalQty
-const total = 50;   // Replace with your actual total
+const total = 50; // Replace with your actual total
 
 updateOrderSummary(totalQty, total);
 
-
-//
-document.addEventListener('DOMContentLoaded', function () {
-  const selectAllCheckbox = document.getElementById('selectAllCheckbox');
-  const totalQtyElement = document.getElementById('totalQty');
-  const totalAmountElement = document.getElementById('totalAmount');
-  const cartItemList = document.getElementById('cart-item-list');
-
-  const cartItems = [
-    // Replace with your cart items
-  ];
-
-  const items = [
-    // Replace with your items
-  ];
-
-  const checkedItems = cartItems.map((el) => el.itemId);
-
-  selectAllCheckbox.addEventListener('change', function () {
-    const isChecked = selectAllCheckbox.checked;
-    updateCheckboxes(isChecked);
-  });
-
-  function updateCheckboxes(isChecked) {
-    const checkboxes = cartItemList.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach(function (checkbox) {
-      checkbox.checked = isChecked;
-    });
-    checkedItems.length = 0;
-
-    if (isChecked) {
-      cartItems.forEach(function (item) {
-        checkedItems.push(item.itemId);
-      });
-    }
-  }
-
-  function updateTotal() {
-    let totalQty = 0;
-    let totalAmount = 0;
-
-    cartItems.forEach(function (cartItem) {
-      if (checkedItems.includes(cartItem.itemId)) {
-        const item = items.find((item) => item.id === cartItem.itemId);
-        if (item) {
-          totalQty += cartItem.quantity;
-          totalAmount += cartItem.quantity * item.price;
-        }
-      }
-    });
-
-    totalQtyElement.textContent = totalQty + ' Items';
-    totalAmountElement.textContent = totalAmount + ' CHF';
-  }
-
-  function handleCheckChange(e, id) {
-    const isChecked = e.target.checked;
-    if (isChecked) {
-      checkedItems.push(id);
-    } else {
-      const index = checkedItems.indexOf(id);
-      if (index !== -1) {
-        checkedItems.splice(index, 1);
-      }
-    }
-    updateTotal();
-  }
-
-  function renderCartItem(item, quantity) {
-    const cartItem = document.createElement('div');
-    cartItem.className = 'cart-item';
-    // Create and append elements for the cart item, including checkboxes, quantity input, delete button, etc.
-    // Add event listeners for checkboxes, quantity input, and delete button
-    // Append cartItem to cartItemList
-  }
-
-  // Render cart items
-  cartItems.forEach(function (cartItem) {
-    const item = items.find((item) => item.id === cartItem.itemId);
-    if (item) {
-      renderCartItem(item, cartItem.quantity);
-    }
-  });
-
-  // Initialize the total
-  updateTotal();
-});
-
-
-
-
-
-
-
-
+// Existing code for rendering the cart items, handling checkboxes, etc.
--- assets/js/korean.js
+++ assets/js/korean.js
@@ -1,36 +1,34 @@
-const koreans = [
+const items = [
     { "id": 1, name: 'Learn Hangul, Korean alphabet', detail: 'We will learn how to read and write Korean', price: '50 CHF/hour', imgSrc: '/assets/images/hangul1.jpg'},
     { "id": 2, name: 'Learn Korean language (any level)', detail: 'You will learn how to speak Korean elegantly', price: '50 CHF/hour', imgSrc: '/assets/images/hangul.jpg' },
     { "id": 3, name: 'Learn Korean with K-pop', detail: 'You can bring any K-pop song you like', price: '50 CHF/hour', imgSrc: '/assets/images/kpop.jpg' },
     { "id": 4, name: 'Learn Korean with K-drama', detail: 'You can bring any K-drama/Korean film you like', price: '50 CHF/hour', imgSrc: '/assets/images/drama.jpg' },
     ];
 
-const koreanContainer = document.getElementById('korean-container');
+const itemContainer = document.getElementById('item-container');
 
-koreans.forEach(function (korean) {
-    let koreanCard = `
+items.forEach(function (item) {
+    let itemCard = `
          <div class="d-flex mb-4">
-         <div class="korean-wrap d-flex flex-column">
-         <img src="${korean.imgSrc}" class="korean-photo" alt="${korean.name}'s picture">
-         <h5 class="card-title1">${korean.name}</h5>
+         <div class="item-wrap d-flex flex-column">
+         <img src="${item.imgSrc}" class="item-photo" alt="${item.name}'s picture">
+         <h5 class="card-title1">${item.name}</h5>
          </div>
             <div class="info d-none">
-                    <h5 class="card-title">${korean.name}</h5>
-                    <div class="card-text mt-4"><strong>Detail:</strong> ${korean.detail}</div>
-                    <div class="card-text"><strong>Price:</strong> ${korean.price}</div>
-                    <button className="item-button">Add to cart</button>
+                    <h5 class="card-title">${item.name}</h5>
+                    <div class="card-text mt-4"><strong>Detail:</strong> ${item.detail}</div>
+                    <div class="card-text"><strong>Price:</strong> ${item.price}</div>
+                    <button class="item-button">Add to cart</button>
 
 </div>
          </div>
        
     `;
     
-    koreanContainer.insertAdjacentHTML('beforeend', koreanCard);
+    itemContainer.insertAdjacentHTML('beforeend', itemCard);
 });
 
-// Existing code ...
-
-let clickElements = document.getElementsByClassName('korean-photo');
+let clickElements = document.getElementsByClassName('item-photo');
 let targetElements = document.getElementsByClassName('info');
 let addToCartButtons = document.getElementsByClassName('item-button');
 
@@ -38,19 +36,19 @@ for (let i = 0; i < clickElements.length; i++) {
     clickElements[i].addEventListener('click', function () {
         hideAllExcept(i);
         targetElements[i].classList.toggle('d-none');
-        console.log(koreans[i].name);
+        console.log(items[i].name);
     });
 
-    addToCartButtons[i].addEventListener('click', function () {
+    targetElements[i].querySelector('.item-button').addEventListener('click', function () {
         // Get the ID of the item associated with the clicked "Add to cart" button
-        const itemId = koreans[i].id;
+        const itemId = items[i].id;
 
         // Add the item ID to local storage (you can customize the key and storage logic)
         localStorage.setItem('cartItemIds', JSON.stringify([...getCartItemIdsFromStorage(), itemId]));
     });
 
     clickElements[i].addEventListener('mouseout', function () {
-        clickElements[i].src = koreans[i].imgSrc;
+        clickElements[i].src = items[i].imgSrc;
     });
 }
 
