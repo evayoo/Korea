d23171eb98e20cf71b568651fd286c317e5a3b3b
Chaewon <chaewon.yoo@powers.org>
2023-11-08 15:54:34 +0100

    update cart

--- assets/js/ShoppingCart.js
+++ assets/js/ShoppingCart.js
@@ -1,4 +1,4 @@
-// Function to create a cart item element
+// Function to create a cart item element . object of item
 function createCartItemElement(
   item,
   checkedItems,
@@ -22,7 +22,7 @@ function createCartItemElement(
   thumbnail.className = "cart-item-thumbnail";
 
   const img = document.createElement("img");
-  img.src = item.img;
+  img.src = item.imgSrc;
   img.alt = item.name;
 
   thumbnail.appendChild(img);
@@ -74,22 +74,29 @@ function createCartItemElement(
 }
 
 // Usage:
-const cart = document.getElementById("cart");
-let item = {
-  id: 1,
-  img: "../../assets/images/bibim.jpg",
-  name: "Bibimbap",
-  price: 30 // Add item detail
-};
+
 const checkedItems = [];
 const quantity = 1;
 
-console.log(item);
+const cart = document.getElementById("cart");
+
+console.log(localStorage.getItem('cartItemIds'));
+
+let listOFId = JSON.parse(localStorage.getItem('cartItemIds'));
+
+const items = [
+  { id: 1, name: 'Learn Hangul, Korean alphabet', detail: 'We will learn how to read and write Korean', price: 50, imgSrc: '/assets/images/hangul1.jpg'},
+  { id: 2, name: 'Learn Korean language (any level)', detail: 'You will learn how to speak Korean elegantly', price: 50, imgSrc: '/assets/images/hangul.jpg' },
+  { id: 3, name: 'Learn Korean with K-pop', detail: 'You can bring any K-pop song you like', price: 50, imgSrc: '/assets/images/kpop.jpg' },
+  { id: 4, name: 'Learn Korean with K-drama', detail: 'You can bring any K-drama/Korean film you like', price: 50, imgSrc: '/assets/images/drama.jpg' },
+  ];
+
+//console.log(item);
 handleCheckChange = function () {};
 handleQuantityChange = function () {};
 handleDelete = function () {};
 const cartItem = createCartItemElement(
-  item,
+  items,
   checkedItems,
   handleCheckChange,
   handleQuantityChange,
@@ -98,22 +105,53 @@ const cartItem = createCartItemElement(
 );
 cart.appendChild(cartItem);
 
+listOFId.forEach(idFromLocalStorage =>{
+  items.forEach((service) => {
+     if( service.id === idFromLocalStorage){
+      let cartItem = createCartItemElement(
+        service,
+        checkedItems,
+        handleCheckChange,
+        handleQuantityChange,
+        handleDelete,
+        quantity
+      );
+      cart.appendChild(cartItem);
+     }
+  })
+
+})
+
+// let idGroup = listOFId.reduce((acc, cur) => {
+//   acc[cur] += 1;
+// }, []);
+
+// console.log(idGroup);
+
+
 // Order summary
 
-function updateOrderSummary(totalQty, total) {
+function updateOrderSummary(totalQty, totalAmount) {
   const totalQtyElement = document.getElementById("totalQty");
   const totalAmountElement = document.getElementById("totalAmount");
 
   if (totalQtyElement && totalAmountElement) {
     totalQtyElement.textContent = totalQty + " Items";
-    totalAmountElement.textContent = total + " CHF";
+    totalAmountElement.textContent = totalAmount + " CHF";
   }
 }
-
 // Usage:
-const totalQty = 5; // Replace with your actual totalQty
-const total = 50; // Replace with your actual total
+// const totalQty = 5; // Replace with your actual totalQty
+// const totalAmount = 50; // Replace with your actual totalAmount
 
-updateOrderSummary(totalQty, total);
+let totalQty = listOFId.length;
+let totalAmount = 0;
+for (let i = 0; i < listOFId.length; i++) {
+  console.log(items[listOFId[i]].price)
+  totalAmount += items[listOFId[i]].price;
+}
+console.log(totalQty, totalAmount)
+console.log(listOFId)
 
+updateOrderSummary(totalQty, totalAmount);
 // Existing code for rendering the cart items, handling checkboxes, etc.
--- assets/js/korean.js
+++ assets/js/korean.js
@@ -1,8 +1,8 @@
 const items = [
-    { "id": 1, name: 'Learn Hangul, Korean alphabet', detail: 'We will learn how to read and write Korean', price: '50 CHF/hour', imgSrc: '/assets/images/hangul1.jpg'},
-    { "id": 2, name: 'Learn Korean language (any level)', detail: 'You will learn how to speak Korean elegantly', price: '50 CHF/hour', imgSrc: '/assets/images/hangul.jpg' },
-    { "id": 3, name: 'Learn Korean with K-pop', detail: 'You can bring any K-pop song you like', price: '50 CHF/hour', imgSrc: '/assets/images/kpop.jpg' },
-    { "id": 4, name: 'Learn Korean with K-drama', detail: 'You can bring any K-drama/Korean film you like', price: '50 CHF/hour', imgSrc: '/assets/images/drama.jpg' },
+    { "id": 1, name: 'Learn Hangul, Korean alphabet', detail: 'We will learn how to read and write Korean', price: 50 , imgSrc: '/assets/images/hangul1.jpg'},
+    { "id": 2, name: 'Learn Korean language (any level)', detail: 'You will learn how to speak Korean elegantly', price: 50 , imgSrc: '/assets/images/hangul.jpg' },
+    { "id": 3, name: 'Learn Korean with K-pop', detail: 'You can bring any K-pop song you like', price: 50 , imgSrc: '/assets/images/kpop.jpg' },
+    { "id": 4, name: 'Learn Korean with K-drama', detail: 'You can bring any K-drama/Korean film you like', price: 50 , imgSrc: '/assets/images/drama.jpg' },
     ];
 
 const itemContainer = document.getElementById('item-container');
@@ -68,5 +68,6 @@ function getCartItemIdsFromStorage() {
     return cartItemIds ? JSON.parse(cartItemIds) : [];
 }
 
+// 만약에 앞서 선택한 항목을 한 번 더 클릭하면, 카드가 추가 되지 않는다. 
 
 
